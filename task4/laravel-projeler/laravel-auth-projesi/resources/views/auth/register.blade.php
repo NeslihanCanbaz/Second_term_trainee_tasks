6bd899aaaad36068ef6d30ff4668fca7bb9e4bc7
neslihan <neslicnbzz@gmail.com>
2026-07-23 00:53:47 +0200

    Add optional phone number field to registration

--- task4/laravel-projeler/laravel-auth-projesi/resources/views/auth/register.blade.php
+++ task4/laravel-projeler/laravel-auth-projesi/resources/views/auth/register.blade.php
@@ -15,6 +15,12 @@
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+        <!-- Phone -->
+<div class="mt-4">
+    <x-input-label for="phone" :value="__('Phone Number')" />
+    <x-text-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" autocomplete="tel" />
+    <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+</div>
 
         <!-- Password -->
         <div class="mt-4">
@@ -26,6 +32,9 @@
                             required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <p class="mt-2 text-sm text-gray-500">
+    Password must be at least 8 characters.
+</p>       
         </div>
 
         <!-- Confirm Password -->
